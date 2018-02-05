1847c0485602b76c3304e73ed3bd1c92fe199266
dukkevin <dukkevin@gmail.com>
2018-02-05 23:08:50 +0200

    Added bonus question.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.quizapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -19,6 +20,9 @@ public class MainActivity extends AppCompatActivity {
         fourthRadioGroup = (RadioGroup) findViewById(R.id.fourth_radio_group);
         fifthRadioGroup = (RadioGroup) findViewById(R.id.fifth_radio_group);
         sixthRadioGroup = (RadioGroup) findViewById(R.id.sixth_radio_group);
+        firstCheckBox = (CheckBox) findViewById(R.id.first_checkbox);
+        secondCheckBox = (CheckBox) findViewById(R.id.second_checkbox);
+        thirdCheckBox = (CheckBox) findViewById(R.id.third_checkbox);
 
     }
 
@@ -29,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup fourthRadioGroup;
     private RadioGroup fifthRadioGroup;
     private RadioGroup sixthRadioGroup;
-    private RadioButton firstRadioButton;
+    private CheckBox firstCheckBox;
+    private CheckBox secondCheckBox;
+    private CheckBox thirdCheckBox;
 
     /**
      * Reset all answers
@@ -42,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
         fourthRadioGroup.clearCheck();
         fifthRadioGroup.clearCheck();
         sixthRadioGroup.clearCheck();
+        firstCheckBox.setChecked(false);
+        secondCheckBox.setChecked(false);
+        thirdCheckBox.setChecked(false);
     }
 
     /**
@@ -55,7 +64,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void showGoodAnswers(View view) {
         resetData();
-        firstRadioButton.setChecked(true);
+
     }
 }
 
